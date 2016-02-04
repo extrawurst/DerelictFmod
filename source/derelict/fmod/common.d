a5381f71b70551d3be93ea9dac947e04c16157bc
Stephan Dilly <dilly.stephan@gmail.com>
2016-02-04 20:42:54 +0100

    update copyrights

--- source/derelict/fmod/common.d
+++ source/derelict/fmod/common.d
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Derelict Developers
+ * Copyright (c) 2016 Derelict Developers
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
